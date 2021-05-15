433e737217de5ee0db6ecc4d18b55d63ac57afdb
NoePS <noe-ps@hotmail.com>
2021-05-15 10:24:16 -0500

    Tabla/Nav
    Front of the Table comp and NavBar comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
